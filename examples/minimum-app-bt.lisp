c0a2e5e5d0380403e2065731be12135a8e437e4f
Madhu <enometh@net.meer>
2025-05-25 06:21:31 +0530

    gficl - integrate with bordeaux threads
    * src/base-app-bt.lisp: build on
    base-app.lisp. (gficl-app:threaded-executor-mixin): new
    class. provides facility to execute code in the main
    thread. (gficl-app:launch): wrap up gficl-app:start for housekeeping
    threads.  (gifcl-app:{apply-in-thread,in-thread}): schedule thunks.
    
    * examples/minimum-app-bt.lisp: builds on minimum-app.lisp.
    
    [250811] src/base-app-bt.lisp: export BASE-APP-BT
    
    [251230]
    * gficl.system: (gficl-examples-bt): don't depend on
    gficl-examples (and transitively wavefront, etc.), just include
    minumum.lisp in the component list.

---
+++ examples/minimum-app-bt.lisp
@@ -0,0 +1,22 @@
+(in-package :gficl-examples/minimum-app)
+
+(defclass minimum-app-bt (minimum-app gficl-app:threaded-executor-mixin) ())
+
+#||
+(setq $up '(((0 0.9)) ((-0.9 -0.9)) ((0.9 -0.9))))
+(setq $dn '(((0 -0.9)) ((0.9 0.9)) ((-0.9 0.9))))
+
+(setq $t1 (make-instance 'minimum-app-bt))
+(gficl-app:launch $t1)
+
+(gficl-app:in-thread
+  (setf (slot-value $t1 'vertices) $dn)
+  (signal 'gficl-app:restart-pipeline))
+
+(gficl-app:apply-in-thread $t1 (lambda () (signal 'gficl-app:quit)))
+(gficl-app:apply-in-thread $t1 (lambda ()
+				 (setf (slot-value $t1 'vertices) $up)
+				   (signal 'gficl-app:restart-pipeline)
+				 (format t "WILLIE ~A~&" (bt:current-thread))))
+||#
+
---
+++ src/base-app-bt.lisp
@@ -0,0 +1,91 @@
+;;; -*- Mode: LISP; Package: :cl-user; BASE: 10; Syntax: ANSI-Common-Lisp; -*-
+;;;
+;;;   Time-stamp: <>
+;;;   Touched: Fri May 23 14:30:13 2025 +0530 <enometh@net.meer>
+;;;   Bugs-To: enometh@net.meer
+;;;   Status: Experimental.  Do not redistribute
+;;;   Copyright (C) 2025 Madhu.  All Rights Reserved.
+;;;
+;;; integrate GFICL-APP with Bordeaux Threads.
+
+;;; mixin GFICL-APP:THREADED-EXECUTOR-MIXIN into your subclass of
+;;; GFICL-APP:BASE-APP and you can use GFICL-APP:APPLY-IN-THREAD to
+;;; schedule lisp thunks to run in the same thread that %glfw:init was
+;;; called during the next GFICL-APP:UPDATE step.
+;;;
+;;; use GFICL-APP:LAUNCH to start the app in it's own thread and the
+;;; macro GFICL-APP:IN-THREAD to schedule execution of forms within
+;;; the current app's main thread., or use GFICL-APP:BASE-APP-BT which
+;;; already does this for you.
+
+(in-package "GFICL")
+
+#+nil
+(require 'bordeaux-threads)
+
+(eval-when (:load-toplevel :compile-toplevel :execute)
+  (mapcar (lambda (x)
+	    (export (intern x "GFICL-APP") "GFICL-APP"))
+	  '("THREADED-EXECUTOR-MIXIN"
+	    "APPLY-IN-THREAD"
+	    "LAUNCH"
+	    "*APP*"
+	    "IN-THREAD"
+	    "RESTART-PIPELINE"
+	    "SHUTDOWN"
+	    "BASE-APP-BT")))
+
+(defclass gficl-app:threaded-executor-mixin ()
+  ((main-thread :initform nil)
+   (task-queue :initform nil)
+   (task-queue-lock :initform (bt:make-lock))))
+
+(defmethod gficl-app:start :before ((app gficl-app:threaded-executor-mixin)
+				    &key &allow-other-keys)
+  (with-slots (main-thread) app
+    (setq main-thread (bt:current-thread))))
+
+(defmethod gficl-app:update-fn :before ((app gficl-app:threaded-executor-mixin))
+  (with-slots (task-queue-lock task-queue) app
+    (bt:with-lock-held (task-queue-lock)
+      (loop for thunk = (pop task-queue)
+	    while thunk do
+	    (restart-case (funcall thunk)
+	      (cont ()
+		;; XXX if you ABORT and don't exit via this CONT
+		;; RESTART I *guarantee* your lisp will be hosed.
+		:report "ignore the error encountered when executing task")
+	      (blow-queue ()
+		:test (lambda (c) (declare (ignore c)) (> (length task-queue) 0))
+		:report "ignore the error encountered when executing task and clear the current task queue"
+		(setq task-queue nil)))))))
+
+(defmethod gficl-app:apply-in-thread ((app gficl-app:threaded-executor-mixin) func &rest args)
+  (with-slots (task-queue-lock task-queue) app
+    (bt:with-lock-held (task-queue-lock)
+      (push (lambda () (apply func args)) task-queue))))
+
+(defvar gficl-app:*app* nil "Current GFICL-APP")
+
+(defmethod gficl-app:launch ((app gficl-app:threaded-executor-mixin) &rest args &key &allow-other-keys)
+  (bt:make-thread (lambda ()
+		    (setq gficl-app:*app* app)
+		    (apply #'gficl-app:start app args))
+		  :name (format nil "gficl-main-runner for ~A" app)))
+
+(defmacro gficl-app:in-thread (&body body)
+  `(progn (check-type gficl-app:*app* gficl-app:threaded-executor-mixin)
+	  (with-slots (main-thread) gficl-app:*app*
+	    (assert (bt:thread-alive-p main-thread)))
+	  (gficl-app:apply-in-thread gficl-app:*app* (lambda () ,@body))))
+
+(defun gficl-app:shutdown ()
+  (gficl-app:in-thread  (signal 'gficl-app:quit)))
+
+(defun gficl-app:restart-pipeline ()
+  (gficl-app:in-thread (signal 'gficl-app:restart-pipeline)))
+
+
+(defclass gficl-app:base-app-bt
+    (gficl-app:base-app gficl-app:threaded-executor-mixin)
+  ())
