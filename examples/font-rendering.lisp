466f2d347af6c6b067169d060ae48e527c6b074b
Madhu <enometh@net.meer>
2025-12-11 16:35:10 +0530

    examples/font-rendering.lisp: (run): pass a function to with-window via :resize-callback
    the slot in gficl::render-statestate is declared as a function not a
    function designator so ccl won't accept 'resize here

--- examples/font-rendering.lisp
+++ examples/font-rendering.lisp
@@ -80,7 +80,7 @@ void main() {
 
 (defun run ()
   (gficl:with-window
-   (:title "font rendering" :width 600 :height 400 :resize-callback 'resize)
+   (:title "font rendering" :width 600 :height 400 :resize-callback #'resize)
    (setup)
    (loop until (gficl:closedp)
 	 do (update)
