bdca2e1b8e270299917dd8bc1c3fb9ea2ca7d893
Noam Zeise <noam4134@gmail.com>
2026-02-22 15:19:01 +0000

    updated mat

--- src/matrix.lisp
+++ src/matrix.lisp
@@ -262,25 +262,6 @@ fov is in radians."
     (gficl:scale-matrix
      (list (if (< ratio 1) ratio 1) (if (> ratio 1) (/ 1 ratio) 1) 1))))
 
-(declaim (ftype (function (vec
-			   &key (:depth number) (:rotation number) (:pivot vec)))
-		2d-rect-matrix))
-(defun 2d-rect-matrix (rect &key (depth 0) (rotation 0) (pivot (gficl:make-vec '(0 0))))
-  (assert (= (dimension rect) 4) ()
-	  "~% rect vec must have 4 components: (x y w h)" rect)
-  (destructuring-bind (x y w h) (vec-data rect)
-    (gficl:*mat
-     (gficl:translation-matrix (list x y depth))
-     (let ((px (gficl:vec-ref pivot 0))
-	   (py (gficl:vec-ref pivot 1)))
-       (if (not (= rotation 0))
-	   (gficl:*mat
-	    (gficl:translation-matrix (list px py 0))
-	    (gficl:2d-rotation-matrix rotation)
-	    (gficl:translation-matrix (list (- px) (- py) 0)))
-	 (gficl:make-matrix)))
-     (gficl:scale-matrix (list w h 1)))))
-
 ;;; --- set shader matrices ---
 
 (defmacro with-foreign-matrix ((var matrix) &body body)
